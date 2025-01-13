fd60d903502b795c46b96fc15256bbca30bd5ae1
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2025-01-12 21:27:33 -0300

    adding vite config settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "BirdSanctuary",
+  build: {
+    assetsDir: "assets",
+    sourcemap: true,
+  },
   plugins: [react()],
-})
+});
