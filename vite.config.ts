97541aeab2978c504ed8bec98b15e756920da801
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2025-01-23 13:47:50 -0300

    few changes + git ignore update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+
+
+const root = path.resolve(__dirname, "./");
+const publicDir = path.resolve(__dirname, "./public/");
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    assetsDir: "assets",
-    sourcemap: true,
-  },
   plugins: [react()],
+  root,
+  publicDir,
+  base : process.env.NODE_ENV === 'production' ? "/" : "/BirdSanctuary/",
+  build: {
+    target: "esnext",
+    emptyOutDir: true,
+    assetsDir: "assets",
+  },
+  
 });
